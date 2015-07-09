fc604c426b550e9e801871cae20c300f983ba38e
dantasfelipe <raam0sfelipe@gmail.com>
2015-07-09 10:18:56 -0300

    igual ao video-tutorial.

--- bfgeo/urls.py
+++ bfgeo/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url, patterns
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -10,7 +11,7 @@ urlpatterns = [
 
 if settings.DEBUG:
 	urlpatterns += patterns('',
-		(r'^media/$', 'django.views.static',
+		(r'^media/(P<path>.*)$', 'django.views.static.serve',
 		 {'document_root': settings.MEDIA_ROOT, 'show_indexex': True}),
 	)
 
